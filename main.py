f3c8b51794ac1b9b37212fb9cddc48ed8639c4da
hfoxwell <62626942+hfoxwell@users.noreply.github.com>
2024-08-19 15:17:37 +1000

    Added Module files, removed unnessicary module file

--- main.py
+++ main.py
@@ -9,11 +9,11 @@
 ###
 # Imports
 ###
-
 # Global Packages
+import requests
 
 # Local imports
-
+from src.actions import Authenticator, SMAuthenticator
 # ============ #
 
 ###
@@ -29,6 +29,20 @@
 
 def main():
     """Main entry point for Application"""
+     # Variables
+    res: requests.Response = requests.Response()
+    authenticator: Authenticator = SMAuthenticator(BASE_URL, USERNAME, PASSWORD)
+    
+    # Load Environment Variables
+    
+    # Authenticate with API
+    try:
+        authenticator.authenticate()
+    except requests.exceptions.ConnectionError as e:
+        print("Unable to connect to SM Marks, please check credentials and connection")
+        raise e
+    
+    print(f"Authenticator: {authenticator.session_key= } {authenticator.token_key= }")
 
 
 ###
