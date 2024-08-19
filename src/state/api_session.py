a4a7d1862d86c6f8bee582dccbdaabcf17cf36c3
hfoxwell <62626942+hfoxwell@users.noreply.github.com>
2024-08-19 13:54:48 +1000

    Modified session, with timeout calculated in seconds. Also added local import for authenticator. Added inheritance from Requests session. This may be a bad idea.

--- src/state/api_session.py
+++ src/state/api_session.py
@@ -2,14 +2,37 @@
     Author: Hayden Foxwell
     Date:   08/08/24
     Purpose:
-        A class for the storage of sessions:
+        A class tracking of sessions to the API.
+        
+    Changelog:
+        - [H Foxwell (19/08/24)] 
+            Added session timeout as specified in
+            SM marks documentation. Session expires every 20
+            min requiring a new token and key.
 """
+###
+# MARK: Imports
+###
+# Global imports
+import requests
 
-class session:
+# Local imports
+from .actions.authenticate import Authenticator
+
+# ====================== #
+
+class Session(requests.Session):
     """Represents a session with the API"""
     
-    session_token = ""
+    # Constants    
+    SESSION_TIMEOUT_SEC: int = (20 * 60) # Session times out in 20 min
     
-    def __init__(self, authenticator, ) -> None:
-        # Get an authenticator
+    def __init__(self, authenticator: Authenticator) -> None:
+        # Initialise superclass
+        super().__init__()
+        
+        # Initalise authenticated state to false
+        self.authenticated: bool = False
+        
+        # Get authenticator with which to authenticate
         self.authenticator = authenticator
