890126e29253057b794a948dc9dcaf25539e6d12
hfoxwell <62626942+hfoxwell@users.noreply.github.com>
2024-08-19 13:30:29 +1000

    Created Main file for python application. Added File description and main entrypoint.

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+    Author: Hayden Foxwell
+    Date:   08/08/2024
+    Purpose:
+        To extract marks from SM Marks so they can be easily entered into 
+        other programs for use in the Studies Department
+"""
+###
+# Imports
+###
+
+# Global Packages
+
+# Local imports
+
+
+# ============ #
+
+###
+# Globals
+###
+
+# ============ # 
+
+###
+# Auxiliary functions
+### 
+
+# ============ #
+
+###
+# MARK: Main Function
+### 
+def main():
+    """Main entry point for Application"""
